1d3b4aea412c52f12521392b595dd292aac0d467
hyejin1231 <hyejinbag772@gmail.com>
2022-04-06 10:57:36 +0900

    20220405 #8. Optional

---
+++ src/com/myJava/practice/ch4/App.java
@@ -0,0 +1,31 @@
+package com.myJava.practice.ch4;
+
+import java.time.Duration;
+import java.util.ArrayList;
+import java.util.List;
+
+// Optional
+public class App {
+    public static void main(String[] args) {
+        List<OnlineClass> springClasses = new ArrayList<>();
+        springClasses.add(new OnlineClass(1, "spring boot", true));
+        springClasses.add(new OnlineClass(2, "spring data jpa", true));
+        springClasses.add(new OnlineClass(3, "spring mvc", false));
+        springClasses.add(new OnlineClass(4, "spring core", false));
+        springClasses.add(new OnlineClass(5, "rest api development", false));
+
+//        OnlineClass spring_boot = new OnlineClass(1, "spring boot", true);
+//        Duration studyDuration = spring_boot.getProgress().getStudyDuration();
+//        System.out.println(studyDuration); // ERROR !  Progress가 null
+
+        OnlineClass spring_boot = new OnlineClass(1, "spring boot", true);
+        Progress progress = spring_boot.getProgress();
+        if (progress != null) { // 이런 코드는 에러를 만들기 딱 좋은 코드 null 체크를 까먹을 수 있기 때문이다.
+            System.out.println(progress.getStudyDuration());
+        }
+
+
+
+
+    }
+}
---
+++ src/com/myJava/practice/ch4/OnlineClass.java
@@ -0,0 +1,63 @@
+package com.myJava.practice.ch4;
+
+import java.util.Optional;
+
+public class OnlineClass {
+    private Integer id;
+    private String title;
+    private boolean closed;
+
+    private Progress progress;
+
+//    private Optional<Progress> progress; 이렇게 사용하는건 권장 X
+
+
+    public OnlineClass(Integer id, String title, boolean closed) {
+        this.id = id;
+        this.title = title;
+        this.closed = closed;
+    }
+
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public boolean isClosed() {
+        return closed;
+    }
+
+    public void setClosed(boolean closed) {
+        this.closed = closed;
+    }
+
+    // 어떻게 보면 null을 리턴하는 것 자체가 문제다.
+    public Progress getProgress() {
+        if (this.progress == null) {
+            throw new IllegalStateException(); // null을 체크해서 Exception을 발생시킨다.
+        }
+        return progress;
+    }
+
+    // Optional 이용 -> 리턴 타입으로만 쓰는것이 좋다!
+    // .of() 안에 들어오는 건 null 이 아닐 경우라고 가정! -> null 이면 NPE 발생
+//    public Optional<Progress> getProgress() {
+//        return Optional.ofNullable(progress);
+//    }
+
+    public void setProgress(Progress progress) {
+        this.progress = progress;
+    }
+}
